75c92101e8457c7c11fbd82538287e430e43b8af
CorrectRoadH <a778917369@gmail.com>
2022-09-13 14:37:14 +0800

    fix(test): add test

---
+++ src/components/viusalBlocks/__tests__/VisualBlock.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import VirualBlock from "../VisualBlock";
+import BlockStructInstance from "../../../data/BlockStructInstance";
+import { store } from "../../../store";
+import { Provider } from "react-redux";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+
+it("renders <VisualBlock /> block", () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <DndProvider backend={HTML5Backend}>
+        <VirualBlock
+          blockdata={
+            {
+              title: "hello",
+              argNum: 0,
+              args: [],
+              argsTip: [],
+              componentType: "block",
+              code: `234234`,
+              children: [],
+              childrenInstance: undefined,
+              id: 1,
+              inTop: true,
+            } as BlockStructInstance
+          }
+        />
+      </DndProvider>
+    </Provider>
+  );
+  expect(getByText(`hello\n参数:`)).toBeTruthy();
+});
--- src/pages/__tests__/Home.test.tsx
+++ src/pages/__tests__/Home.test.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { expect, it } from "vitest";
 import { renderWithRouter } from "../../utils/rtl-wrapper";
+import { store } from "../../store";
+import { Provider } from "react-redux";
 
 import Home from "../Home";
+import { render } from "@testing-library/react";
 
 it("renders <Home /> page", () => {
-  const { getByText } = renderWithRouter(<Home />);
-  expect(getByText("Welcome!")).toBeTruthy();
+  const { getByText } = render(
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
+  expect(getByText("area2")).toBeTruthy();
 });
